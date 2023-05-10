492d28d8008ca2f64f77e76a5fd9460c461a3097
Tamar Revazishvili <tamar.d154@gmail.com>
2023-05-10 10:45:43 +0300

    all except iostream

---
+++ sources/Fraction.cpp
@@ -0,0 +1,229 @@
+#include "Fraction.hpp"
+
+#include <numeric>
+#include <cmath>
+
+using namespace ariel;
+using namespace std;
+
+// Default constructor
+Fraction::Fraction() : _numerator(0), _denominator(1) {}
+
+// Constroctor that gets two ints
+Fraction::Fraction(const int &numerator, const int &denominator)
+{
+    if (denominator == 0)
+    {
+        throw "denominator cannot be zero!";
+    }
+
+    if (numerator < 0 && denominator < 0)
+    {
+        this->_numerator = -numerator;
+        this->_denominator = -denominator;
+    }
+    else if (denominator < 0)
+    {
+        this->_numerator = -numerator;
+        this->_denominator = -denominator;
+    }
+    else
+    {
+        this->_numerator = numerator;
+        this->_denominator = denominator;
+    }
+
+    reduceFrac();
+    // cout << "Frac(int, int) constructor: " << this->_numerator << "/" << this->_denominator << endl;
+}
+
+// Constructor that gets a float number
+Fraction::Fraction(const float &num)
+{
+    float num_copy = floatThreeDecimalAcc(num);
+    _numerator = num_copy * 1000;
+    _denominator = 1000;
+    reduceFrac();
+    // cout << "Frac(float) constructor: " << this->_numerator << "/" << this->_denominator << endl;
+}
+
+/*
+    Addition
+*/
+Fraction Fraction::operator+(const Fraction &other) const
+{
+    int numer1 = _numerator;
+    int numer2 = other._numerator;
+    int denom1 = _denominator;
+    int denom2 = other._denominator;
+
+    int commonDenom = denom1 * denom2;
+
+    numer1 *= denom2;
+    numer2 *= denom1;
+
+    int numerator = numer1 + numer2;
+
+    Fraction result(numerator, commonDenom);
+    return result;
+}
+
+Fraction Fraction::operator+(const float &flo) const
+{
+    float accurate_flo = floatThreeDecimalAcc(flo);
+    Fraction frac2(accurate_flo);
+    return *this + frac2;
+}
+
+/*
+    Subtraction
+*/
+
+Fraction Fraction::operator-(const Fraction &other) const
+{
+    int numer1 = _numerator;
+    int numer2 = other._numerator;
+    int denom1 = _denominator;
+    int denom2 = other._denominator;
+
+    int commonDenom = denom1 * denom2;
+
+    numer1 *= denom2;
+    numer2 *= denom1;
+
+    int numerator = numer1 - numer2;
+
+    Fraction result(numerator, commonDenom);
+    return result;
+}
+
+Fraction Fraction::operator-(const float &flo) const
+{
+    float accurate_flo = floatThreeDecimalAcc(flo);
+    Fraction frac2(accurate_flo);
+    return *this - frac2;
+}
+
+/*
+    Multiplication
+*/
+Fraction Fraction::operator*(const Fraction &other) const
+{
+    int numerator = this->_numerator * other._numerator;
+    int denominator = this->_denominator * other._denominator;
+
+    Fraction result(numerator, denominator);
+    return result;
+}
+
+Fraction Fraction::operator*(const float &flo) const
+{
+    float accurate_val = floatThreeDecimalAcc(flo);
+    Fraction frac2(accurate_val);
+    return *this * frac2;
+}
+
+/*
+    Division
+*/
+Fraction Fraction::operator/(const Fraction &other) const
+{
+    int numerator = this->_numerator * other._denominator;
+    int denominator = this->_denominator * other._numerator;
+
+    Fraction result(numerator, denominator);
+    return result;
+}
+
+Fraction Fraction::operator/(const float &flo) const
+{
+    float accurate_val = floatThreeDecimalAcc(flo);
+    Fraction frac2(accurate_val);
+    return *this / frac2;
+}
+
+/*
+    Equals
+*/
+bool Fraction::operator==(const Fraction &other) const
+{
+    bool numerEq = (this->_numerator == other._numerator);
+    bool denomEq = (this->_denominator == other._denominator);
+    return numerEq && denomEq;
+}
+
+bool Fraction::operator==(const float &flo) const
+{
+    Fraction frac(flo);
+    // cout << frac._numerator << "/" << frac._denominator << endl;
+    return *this == Fraction(flo);
+}
+
+/*
+    Bigger than
+*/
+bool Fraction::operator>(const Fraction &other) const
+{
+    int numer1 = this->_numerator * other._denominator;
+    int numer2 = other._numerator * this->_denominator;
+    return numer1 > numer2;
+}
+
+bool Fraction::operator>(const float &flo) const
+{
+    Fraction frac2(flo);
+    return *this > frac2;
+}
+
+/*
+    Increment
+*/
+Fraction Fraction::operator++() // prefix
+{
+    *this = *this + Fraction(1, 1);
+    return *this;
+}
+
+Fraction Fraction::operator++(int postfix) // postfix
+{
+    Fraction copy = *this;
+    *this = *this + Fraction(1, 1);
+    return copy;
+}
+
+/*
+    Decrement
+*/
+Fraction Fraction::operator--() // prefix
+{
+    *this = *this - Fraction(1, 1);
+    return *this;
+}
+
+Fraction Fraction::operator--(int postfix) // postfix
+{
+    Fraction copy = *this;
+    *this = *this - Fraction(1, 1);
+    return copy;
+}
+
+/*
+    Output
+*/
+
+
+
+/*
+    Helper functions
+*/
+
+// This function uses std::gcd on the fraction. if gcd!=1 then the fraction is reduced.
+void Fraction::reduceFrac()
+{
+    int gcd_val = gcd(_numerator, _denominator);
+    if (gcd_val != 1)
+    {
+        _numerator /= gcd_val;
+        _denominator /= gcd_val;
+    }
+}
---
+++ sources/Fraction.hpp
@@ -0,0 +1,143 @@
+#pragma once
+
+#include <iostream>
+const int ACC_FACTOR = 1000.0;
+
+namespace ariel
+{
+    class Fraction
+    {
+
+    private:
+        int _numerator;
+        int _denominator;
+
+    public:
+        /*
+        Constructors
+         */
+        Fraction();
+        Fraction(const int &, const int &);
+        Fraction(const float &);
+
+        /*
+            Getters
+        */
+        int getNumerator() const { return _numerator; }
+
+        int getDenominator() const { return _denominator; }
+
+        /*
+            + oprator
+        */
+        Fraction operator+(const Fraction &other) const;
+        Fraction operator+(const float &flo) const;
+        friend Fraction operator+(const float &flo, const Fraction &frac) { return frac + flo; }
+
+        /*
+            - oprator
+        */
+        Fraction operator-(const Fraction &other) const;
+        Fraction operator-(const float &flo) const;
+        friend Fraction operator-(const float &flo, const Fraction &frac)
+        {
+            Fraction ans = frac - flo;
+            ans._numerator = -ans._numerator;
+            return ans;
+        }
+
+        /*
+         * oprator
+         */
+        Fraction operator*(const Fraction &other) const;
+        Fraction operator*(const float &flo) const;
+        friend Fraction operator*(const float &flo, const Fraction &frac) { return frac * flo; }
+
+        /*
+            / oprator
+        */
+        Fraction operator/(const Fraction &other) const;
+        Fraction operator/(const float &flo) const;
+        friend Fraction operator/(const float &flo, const Fraction &frac)
+        {
+            Fraction frac1(flo);
+            return frac1 / frac;
+        }
+
+        /*
+            == oprator
+        */
+        bool operator==(const Fraction &other) const;
+        bool operator==(const float &flo) const;
+        friend bool operator==(const float &flo, const Fraction &frac) { return frac == flo; }
+
+        /*
+            > oprator
+        */
+        bool operator>(const Fraction &other) const;
+        bool operator>(const float &flo) const;
+        friend bool operator>(const float &flo, const Fraction &frac)
+        {
+            return Fraction(flo) > frac;
+        }
+
+        /*
+            < oprator
+         */
+        bool operator<(const Fraction &other) const { return other > *this; }
+        bool operator<(const float &flo) const { return flo > *this; }
+        friend bool operator<(const float &flo, const Fraction &frac)
+        {
+            return frac > flo;
+        }
+
+        /*
+            >= oprator
+        */
+        bool operator>=(const Fraction &other) const { return (*this > other) || (*this == other); }
+        bool operator>=(const float &flo) const { return (*this > flo) || (*this == flo); }
+        friend bool operator>=(const float &flo, const Fraction &frac) { return (flo > frac) || (flo == frac); }
+
+        /*
+            <= oprator
+        */
+        bool operator<=(const Fraction &other) const { return (*this < other) || (*this == other); }
+        bool operator<=(const float &flo) const { return (*this < flo) || (*this == flo); }
+        friend bool operator<=(const float &flo, const Fraction &frac) { return (flo < frac) || (flo == frac); }
+
+        /*
+          ++ oprator
+        */
+        Fraction operator++();
+        Fraction operator++(int postfix);
+
+        /*
+           -- oprator
+         */
+        Fraction operator--();
+        Fraction operator--(int postfix);
+
+        /*
+           << oprator
+         */
+        friend std::ostream &operator<<(std::ostream &output, const Fraction &frac)
+        {
+            return (output << frac.getNumerator() << "/" << frac.getDenominator());
+        }
+        friend std::ostream &operator<<(std::ostream &output, const float &flo) { return output << flo; }
+
+        /*
+           >> oprator
+         */
+        friend std::istream &operator>>(std::istream &input, const Fraction &frac) { return input; }
+        friend std::istream &operator>>(std::istream &input, const float &flo) { return input; }
+
+        void reduceFrac(); // friend??
+        static float floatThreeDecimalAcc(float flo)
+        {
+            // std::cout << (int)(flo * 1000) / 1000.0 << std::endl;
+            return static_cast<int>(flo * ACC_FACTOR) / static_cast<float>(ACC_FACTOR);
+
+        } // friend??
+    };
+}
